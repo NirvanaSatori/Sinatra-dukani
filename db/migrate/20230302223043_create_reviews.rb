c5574de434cc73d9ca89ef4abec90d68ff7c12dc
MORRIS <morris24kariuki@gmail.com>
2023-03-03 01:37:18 +0300

    created reviews table

--- db/migrate/20230302223043_create_reviews.rb
+++ db/migrate/20230302223043_create_reviews.rb
@@ -1,4 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
+    create_table :reviews do |t|
+      t.string :rating
+      t.string :comment
+      t.integer :product_id
+      t.integer :user_id
   end
 end
