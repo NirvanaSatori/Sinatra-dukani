48ef9bced1093a985b2a602e15d34c8a7924c16b
MORRIS <morris24kariuki@gmail.com>
2023-03-03 02:19:05 +0300

    added routing methods to the API

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < Sinatra::Base
 
     set :default_content_type, 'application/json'
+
+    get '/products' do
+        product = Product.all 
+        product.to_json
+    end
+
+    get 'reviews' do
+        review= Review.all
+        review.to_json
+    end
+
+    get 'users' do
+        user = User.all
+        user.to_json
+    end
+
+
 end  
